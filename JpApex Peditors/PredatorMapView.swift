4e3097b346b7bdaf1753095f753c3951d586d63b
Aggelos <aggelos.nimfopoulos@gmail.com>
2026-05-24 16:38:24 +0300

    Interactive Map Feature

---
+++ JpApex Peditors/PredatorMapView.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorMapView.swift
+//  JpApex Peditors
+//
+//  Created by Aggelos Nimfopoulos on 24/5/26.
+//
+
+import SwiftUI
+
+struct PredatorMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorMapView()
+}
